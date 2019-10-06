696a71e77ce4ec2931dfc764ed8eee9de1f93e65
varadharajm <varadha212@gmail.com>
2019-10-06 17:11:50 +0530

    mv commit

--- sum.py
+++ sum.py
@@ -1,4 +1,7 @@
 a=int(input("Enter the first value of A"))
 b=int(input("Enter the second value of B"))
-sum=a+b
+c=int(input("Enter the three value of B"))
+d=int(input("Enter the forth value of B"))
+sum=a+b+c+d
+
 print("ADD value is:",sum)
